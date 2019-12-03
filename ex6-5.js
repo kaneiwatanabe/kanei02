fca3b93767627bd0798059e8ee7e828cc925e591
kaneiwatanabe <zm18233@josai.ac.jp>
2019-12-03 02:55:39 +0000

    12/3

---
+++ ex6-5.js
@@ -0,0 +1,13 @@
+var who65 = 'world';
+// var come ='が来た'
+function sayhello66() {
+// var who = 'world';
+   alert('Hello, ' + who65 + '!');
+}
+
+function taro() {
+   who65 = '太郎';
+}
+function hanako(){
+    who65 = '花子';
+}
